f02b8735c7908e50c22c4ee12f90cbc34c5999d8
juanpablomancera <juanpablomanceramartinez@gmail.com>
2022-03-06 17:03:41 +0100

    Added responsiveness to landing and navbar

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,6 @@ import portada from "../images/background.jpg";
 export default function Landing(){
     return (
         <section>
-            <img src={portada} alt ="" className="portada"/>
             <div className="introduction">
                 <h1 className="title">The best stock market screener for finding the most profitable stocks</h1>
                 <h3 className="description">Get started know and find the next superperformance stock </h3>
